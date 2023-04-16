b7090472fff326f48726dc1cf28ee997e53160f4
Salem Nassar <salemnassar5@gmail.com>
2023-04-16 08:35:03 -0500

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import generator
 import PySimpleGUI as sg
 
-sg.Window(title="Meal Plan Generator", layout=[[]], margins=(100,50)).read()
+sg.Window(title="Meal Plan Generator", layout=[[]], margins=(100, 50)).read()
